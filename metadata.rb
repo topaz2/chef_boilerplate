76df878a6c7987737d7709f650df0c500f61808e
topaz2 <topaz2.3333@gmail.com>
2014-04-15 16:53:07 +0900

    Added nodejs

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures boilerplate'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
+depends 'node'
+depends 'nodejs'
 depends 'apt-repo'
 depends 'apache2'
 depends 'database'
