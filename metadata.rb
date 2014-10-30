088a82a7fe5671716f519d3660fbe8af09061f03
topaz2 <topaz2.3333@gmail.com>
2014-10-30 15:59:59 +0900

    Remove obsolete cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'topaz2@m0n0m0n0.com'
 license 'GPL v3'
 description 'Installs/Configures boilerplate'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.5.3'
+version '0.5.4'
 
 depends 'apache2'
 depends 'chef-dk'
